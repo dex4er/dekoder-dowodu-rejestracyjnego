2f2fc74619f77990cf5b4eb9f61128455a8e3ce2
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-07-29 22:34:50 +0200

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { /* RouterModule, */ RouteReuseStrategy /* , Routes */ } from '@angular/router'
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
+import { IonicModule } from '@ionic/angular'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx'
 import { StatusBar } from '@ionic-native/status-bar/ngx'
 
@@ -15,8 +14,7 @@ import { AppRoutingModule } from './app-routing.module'
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    SplashScreen
   ],
   bootstrap: [AppComponent]
 })
