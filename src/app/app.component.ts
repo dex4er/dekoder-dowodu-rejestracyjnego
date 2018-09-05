809234b90cb2f9e1c73e4205db4966df21b46441
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2018-09-05 20:09:55 +0200

    Check version of webview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core'
 
 import { Platform } from '@ionic/angular'
+
+import { Market } from '@ionic-native/market/ngx'
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 import { SplashScreen } from '@ionic-native/splash-screen/ngx'
 import { StatusBar } from '@ionic-native/status-bar/ngx'
 
-import { WebviewChecker } from '../lib/webview-checker'
+import MobileDetect from 'mobile-detect'
 
 declare interface NavigatorApp {
   exitApp (): void
@@ -17,17 +19,20 @@ declare interface Navigator {
 
 declare var navigator: Navigator
 
+const ANDROID_VERSION_WITH_CHROME_WEBVIEW = 7
+const CHROME_VERSION_WITH_CSS_GRID = 57
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
   constructor (
+    private market: Market,
     private platform: Platform,
     private screenOrientation: ScreenOrientation,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private webviewChecker: WebviewChecker
+    private statusBar: StatusBar
   ) {
     void this.initializeApp()
   }
@@ -35,12 +40,19 @@ export class AppComponent {
   async initializeApp () {
     await this.platform.ready()
 
-    if (this.platform.is('android')) {
-      const webviewVersion = await this.webviewChecker.getWebViewVersion()
-      const webviewMajorVersion = Number(webviewVersion.split('.')[0])
-      if (!webviewMajorVersion || webviewMajorVersion < 57) {
-        alert(`Android System Webview (${webviewVersion}) jest zbyt stary.\n\Proszę spróbować zainstalować nową wersję.`)
-        await this.webviewChecker.openGooglePlayPage()
+    const md = new MobileDetect(window.navigator.userAgent)
+
+    if (md.is('AndroidOS')) {
+      const androidVersion = md.version('Android') || 0
+      const chromeVersion = md.version('Chrome') || 0
+      if (chromeVersion < CHROME_VERSION_WITH_CSS_GRID) {
+        if (androidVersion >= ANDROID_VERSION_WITH_CHROME_WEBVIEW) {
+          alert(`Wersja Google Chrome (${chromeVersion}) jest zbyt stara.\n\nProszę spróbować zainstalować nowszą.`)
+          this.market.open('com.android.chrome')
+        } else {
+          alert(`Wersja Android System Webview (${chromeVersion}) jest zbyt stara.\n\nnProszę spróbować zainstalować nowszą.`)
+          await this.market.open('com.google.android.webview')
+        }
         navigator.app.exitApp()
       }
     }
